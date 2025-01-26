822774f684d0f9ecb3a9fcd26ab018d5c84997e2
ciccio.trung <francesco.trung@gmail.com>
2025-01-26 17:36:30 +0100

    authoritative_node.py updated

--- authoritative_node.py
+++ authoritative_node.py
@@ -11,6 +11,25 @@ import uvicorn
 
 
 
+class AuthoritativeNode(DHTHandler):
+    
+    def __init__(self,dilith_keys_dir="auth_node_keys_dilithium",kyber_keys_dir="auth_node_keys_kyber"):
+        super().__init__(dilith_keys_dir,kyber_keys_dir)
+        
+    def generate_authoritative_node_did_iiot(self,service_endpoint):
+        did = f"did:iiot:vc-issuer"
+        self.generate_did_iiot(id_service="AuthoritativeNode",service_type="vc-issuer",service_endpoint=service_endpoint,did_uri=did)
+        
+    def generate_vc(self,did_sub: str,modbus_operations: list = None):
+        sk = self.dilith_key_manager.get_private_key("k0")
+        # modifiche da fare: il key manager di AuthKademlia deve riconoscere il tipo di algoritmo dalla lunghezza della chiave privata
+        algorithm = "Dilithium-2"
+        #await self.start_dht_service(5000)
+        #result = await self.dht_node.get(key=extract_did_suffix(did_sub))
+        #if not result:
+            #return None
+        return get_vc(did_sub=did_sub,algorithm=algorithm,vc_issuer_sk=sk,modbus_operations=modbus_operations)
+        
 def send_broadcast_message():
     BROADCAST_IP = "172.29.0.255"
     PORT = 7000
@@ -54,30 +73,11 @@ def send_broadcast_message():
         pass
     finally:
         s.close()
-
-class AuthoritativeNode(DHTHandler):
-    
-    def __init__(self,dilith_keys_dir="auth_node_keys_dilithium",kyber_keys_dir="auth_node_keys_kyber"):
-        super().__init__(dilith_keys_dir,kyber_keys_dir)
         
-    def generate_authoritative_node_did_iiot(self,service_endpoint):
-        did = f"did:iiot:vc-issuer"
-        self.generate_did_iiot(id_service="AuthoritativeNode",service_type="vc-issuer",service_endpoint=service_endpoint,did_uri=did)
         
-    def generate_vc(self,did_sub: str,modbus_operations: list = None):
-        sk = self.dilith_key_manager.get_private_key("k0")
-        # modifiche da fare: il key manager di AuthKademlia deve riconoscere il tipo di algoritmo dalla lunghezza della chiave privata
-        algorithm = "Dilithium-2"
-        #await self.start_dht_service(5000)
-        #result = await self.dht_node.get(key=extract_did_suffix(did_sub))
-        #if not result:
-            #return None
-        return get_vc(did_sub=did_sub,algorithm=algorithm,vc_issuer_sk=sk,modbus_operations=modbus_operations)
-        
-
 auth_node_service = FastAPI()
 auth_node = AuthoritativeNode()
-#peers = send_broadcast_message()
+
 
 class VCRequest(BaseModel):
     did_sub: str
@@ -100,17 +100,21 @@ async def generate_vc(did_sub: str, modbus_operations: Optional[List[str]] = Que
                             
                             
         
-async def configure_auth_node(auth_node: AuthoritativeNode):
+async def configure_auth_node(auth_node: AuthoritativeNode, peers):
     auth_node.generate_authoritative_node_did_iiot("172.29.0.2:5007")
     await auth_node.start_dht_service(5000)
-    await auth_node.dht_node.bootstrap([("172.29.0.101",5000),("172.29.0.8",5000)])
+    await auth_node.dht_node.bootstrap(peers)
     await auth_node.insert_did_document_in_the_DHT()
     await auth_node.dht_node.stop()
     
 if __name__ == "__main__":
+    peers = []
+    while not peers:
+        peers = send_broadcast_message()
+        time.sleep(7)
+        
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    time.sleep(80)
     loop.run_until_complete(configure_auth_node(auth_node))
     # Avvia FastAPI
     uvicorn.run(auth_node_service, host="0.0.0.0", port=5007)
