3bbd39c9f71ae23657fe0d870b8db1e43ece6c6c
fratrung <163033263+fratrung@users.noreply.github.com>
2025-03-21 22:35:15 +0100

    Update issuer_node.py

--- issuer_node.py
+++ issuer_node.py
@@ -10,7 +10,7 @@ import threading
 
 class IssuerNode(DHTHandler):
     
-    def __init__(self,dilith_keys_dir="auth_node_keys_dilithium",kyber_keys_dir="auth_node_keys_kyber"):
+    def __init__(self,dilith_keys_dir="issuer_node_keys_dilithium",kyber_keys_dir="issuer_node_keys_kyber"):
         super().__init__(dilith_keys_dir,kyber_keys_dir)
         
     
@@ -32,21 +32,21 @@ class IssuerNode(DHTHandler):
         
 
 
-auth_node_service = FastAPI()
-auth_node = IssuerNode()
+issuer_node_service = FastAPI()
+issuer_node = IssuerNode()
 
 
 class VCRequest(BaseModel):
     did_sub: str
     modbus_operations: Optional[List[str]] = []
 
-@auth_node_service.get("/get-vc")
+@issuer_node_service.get("/get-vc")
 async def generate_vc(did_sub: str, modbus_operations: Optional[List[str]] = Query(default=[])):
     if not did_sub:
         raise HTTPException(status_code=400, detail="Missing 'did_sub' parameter")
 
     try:
-        result = auth_node.generate_vc(did_sub, modbus_operations)
+        result = issuer_node.generate_vc(did_sub, modbus_operations)
         if result is None:
             raise HTTPException(status_code=404, detail="No result found")
         return JSONResponse(content=result)
@@ -55,12 +55,12 @@ async def generate_vc(did_sub: str, modbus_operations: Optional[List[str]] = Que
                             
                             
         
-async def configure_auth_node(auth_node: IssuerNode, peers):
-    auth_node.generate_issuer_node_did_iiot("172.29.0.2:5007")
-    await auth_node.start_dht_service(5000)
+async def configure_issuer_node(issuer_node: IssuerNode, peers):
+    issuer_node.generate_issuer_node_did_iiot("172.29.0.2:5007")
+    await issuer_node.start_dht_service(5000)
 
     while True:
-        routing_table_kademlia = auth_node.dht_node.protocol.router
+        routing_table_kademlia = issuer_node.dht_node.protocol.router
         all_nodes = []
         for bucket in routing_table_kademlia.buckets:
             all_nodes.extend(bucket.get_nodes())
@@ -69,25 +69,25 @@ async def configure_auth_node(auth_node: IssuerNode, peers):
         await asyncio.sleep(0.5)
 
     print(all_nodes)
-    await auth_node.insert_did_document_in_the_DHT()
+    await issuer_node.insert_did_document_in_the_DHT()
     await asyncio.sleep(1)
-    await auth_node.dht_node._refresh_table()
+    await issuer_node.dht_node._refresh_table()
     while True:
         await asyncio.sleep(5)
 
 async def start_fastapi_service():
-    uvicorn.run(auth_node_service, host="0.0.0.0", port=5007, loop="asyncio")
+    uvicorn.run(issuer_node_service, host="0.0.0.0", port=5007, loop="asyncio")
 
-async def start_main(auth_node,bootstrap_nodes):
+async def start_main(issuer_node,bootstrap_nodes):
     await asyncio.gather(
-        configure_auth_node(auth_node,bootstrap_nodes),
+        configure_issuer_node(issuer_node,bootstrap_nodes),
         start_fastapi_service()
     )  
 
-def conf_auth_node(auth_node,bootstrap_nodes):
+def conf_issuer_node(issuer_node,bootstrap_nodes):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(configure_auth_node(auth_node,bootstrap_nodes))  
+    loop.run_until_complete(configure_issuer_node(issuer_node,bootstrap_nodes))  
 
 if __name__ == "__main__":
     print("Issuer Node started!")
@@ -96,9 +96,9 @@ if __name__ == "__main__":
 
     bootstrap_nodes = [("172.29.0.181",5000),("172.29.0.63",5000)] #entry nodes for DHT  
     
-    dht_thread = threading.Thread(target=conf_auth_node,args=(auth_node,bootstrap_nodes),daemon=True)
+    dht_thread = threading.Thread(target=conf_issuer_node,args=(issuer_node,bootstrap_nodes),daemon=True)
     dht_thread.start()
-    uvicorn.run(auth_node_service, host="0.0.0.0", port=5007,loop="asyncio")
+    uvicorn.run(issuer_node_service, host="0.0.0.0", port=5007,loop="asyncio")
 
 
 
